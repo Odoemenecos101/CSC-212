d942675b059298aa1afa5667be0b07e40c9f648a
Odoemenecos101 <chukwuemekaodoemene2@gmail.com>
2026-04-21 17:02:23 +0100

    2nd commit

--- main.cpp
+++ main.cpp
@@ -1,111 +1,92 @@
 #include <iostream>
-
 #include <glad/glad.h>
-
 #include <GLFW/glfw3.h>
 
+// Vertex Shader
 const char* vertexShaderSource = "#version 330 core\n"
-
-"layout (location = 0) in vec3 aPos; \n"
-
+"layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
-
 "{\n"
-
-"  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
 
+// Fragment Shader: Changed to Bright Red (1.0, 0.0, 0.0)
 const char* fragmentShaderSource = "#version 330 core\n"
-
-"out vec4 FragColor; \n"
-
+"out vec4 FragColor;\n"
 "void main()\n"
-
 "{\n"
-
-"  FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
-
+"   FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
 "}\n\0";
 
 int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	glfwInit();
+    // Scalene Triangle Coordinates
+    // By ensuring no symmetry in the X or Y coordinates relative to each other, 
+    // we create three sides of different lengths.
+    GLfloat vertices[] =
+    {
+        -0.6f, -0.4f, 0.0f,  // Bottom Left
+         0.4f, -0.7f, 0.0f,  // Bottom Right (lower than the left)
+         0.2f,  0.6f, 0.0f   // Top (shifted right)
+    };
 
+    GLFWwindow* window = glfwCreateWindow(800, 800, "PAU-CSC-212 - Scalene Red", NULL, NULL);
+    if (window == NULL) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    glViewport(0, 0, 800, 800);
 
+    // Shader Setup
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
 
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
+    // Buffer Setup
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
+    while (!glfwWindowShouldClose(window)) {
+        // Background Color: Dark Grey
+        glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "BRUNO", NULL, NULL);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-
-	if (window == NULL)
-
-	{
-
-		std::cout << "Failed to create GLFW window " << std::endl;
-
-		glfwTerminate();
-
-		return -1;
-
-
-
-
-
-	}
-
-	glfwMakeContextCurrent(window);
-
-
-
-	gladLoadGL();
-
-
-
-
-
-
-
-
-
-
-
-	glViewport(0, 0, 800, 800);
-
-
-
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-
-	glClear(GL_COLOR_BUFFER_BIT);
-
-	glfwSwapBuffers(window);
-
-
-
-	while (!glfwWindowShouldClose(window))
-
-	{
-
-		glfwPollEvents();
-
-	}
-
-
-
-
-
-	glfwDestroyWindow(window);
-
-	glfwTerminate();
-
-	return 0;
-
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
